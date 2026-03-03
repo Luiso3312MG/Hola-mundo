ddf6914d1c9471b91c41aad94e23fa0ea42d7ee3
Luiso3312MG <rolinomira@gmail.com>
2026-03-02 23:34:42 -0600

    db

--- server.js
+++ server.js
@@ -14,12 +14,13 @@ app.get("/", (req, res) => {
 });
 
 // Conexión MySQL
-const db = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 db.connect(err => {
