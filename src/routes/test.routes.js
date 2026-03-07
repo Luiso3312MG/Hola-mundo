dab60b7890f57e883897ce64c777b8565a5bfdf3
Luiso3312MG <rolinomira@gmail.com>
2026-03-07 02:47:58 -0600

    sdfrd

---
+++ src/routes/test.routes.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const pool = require("../config/db");
+
+const router = express.Router();
+
+router.get("/email", async (req, res) => {
+
+  try {
+
+    const [rows] = await pool.query(
+      `SELECT code FROM roles LIMIT 1`
+    );
+
+    if (rows.length === 0) {
+      return res.json({
+        ok: false,
+        message: "No hay usuarios"
+      });
+    }
+
+    res.json({
+      ok: true,
+      email: rows[0].email
+    });
+
+  } catch (error) {
+
+    console.error(error);
+
+    res.status(500).json({
+      ok: false,
+      message: "Error en consulta"
+    });
+
+  }
+
+});
+
+module.exports = router;
